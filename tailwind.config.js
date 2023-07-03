1df359e1a1f458e39913e78572c361c70125cce3
Joaquin CR <joaquin2010233@gmail.com>
2023-07-03 16:01:59 -0600

    Create the poject structure and the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,33 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'bgColor-Home': '#19192e',
+        'bgColor-Navbar': '#1f1f39',
+        'Color-M&BTN': '#eea86c'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' }
+        }
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      },
+      fontFamily: {
+        Modern: ['Modern20']
+      }
     },
+
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
